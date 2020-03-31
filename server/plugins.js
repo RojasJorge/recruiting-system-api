b6c6cbe33ecce86a09a0b46e77fe9dec52e6f954
Jorge Rojas <jorge@royalestudios.com>
2020-03-30 20:06:06 -0600

    UPDATE Production conditions.

--- server/plugins.js
+++ server/plugins.js
@@ -1,7 +1,17 @@
 'use strict'
 
-module.exports = [
+const config = require('../config')
+
+let plugins = [
   require('hapi-auth-jwt2').plugin,
   require('@hapi/inert').plugin,
   require('@hapi/vision').plugin
 ]
+
+if (JSON.parse(config.get('/app/secure')))
+  plugins.push({
+    plugin: require('hapi-require-https'),
+    options: {}
+  })
+
+module.exports = plugins
