5cab891b725dd80c7e81011fd8767e68bd3f3ea1
Jorge Rojas <jorge@royalestudios.com>
2020-03-30 20:06:53 -0600

    UPDATE Change to the new global get query.

--- server/handlers/academic_level-handler.js
+++ server/handlers/academic_level-handler.js
@@ -1,12 +1,17 @@
 'use strict'
 
 const queries = require('../queries/academic_level')
+const get = require('../queries/get')
 
 module.exports = {
   get: async (req, h) =>
-    h.response(await queries.get(req.server.db.r, req.server.db.conn, req.query)),
+    h.response(req.params.id ? await get.single(req, 'academic_levels') : await get.collection(req, 'academic_levels')),
   add: async (req, h) =>
     h.response(await queries.add(req.server.db.r, req.server.db.conn, req.payload, req.owner)),
   update: async (req, h) =>
-    h.response(await queries.update(req.server.db.r, req.server.db.conn, req.payload))
+  {
+    console.log("log on update", req.params.id, req.payload)
+    return h.response(true)
+    // h.response(await queries.update(req.server.db.r, req.server.db.conn, req.params.id, req.payload))
+  }
 }
