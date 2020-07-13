829a1d50509b72d0c43adec076be9f451ea31630
Jorge Rojas <jorge.alberto.rojas.solorzano@gmail.com>
2020-07-13 12:04:14 -0600

    UPDATE reindent

--- server/routes/company.js
+++ server/routes/company.js
@@ -10,84 +10,85 @@ const schemas = require('../schemas')
  */
 
 const Company = [{
-  method: 'POST',
-  path: config.get('/api/base_path') + '/company',
-  handler: handlers.company.add,
-  options: {
-    auth: {
-      scope: ['admin', 'umana', 'company', 'candidate']
-    },
-    validate: {
-      payload: schemas.company._add
-    }
-  }
+	method: 'POST',
+	path: config.get('/api/base_path') + '/company',
+	handler: handlers.company.add,
+	options: {
+		auth: {
+			scope: ['admin', 'umana', 'company', 'candidate']
+		},
+		validate: {
+			payload: schemas.company._add
+		}
+	}
 }, {
-  method: 'GET',
-  path: config.get('/api/base_path') + '/company/{id?}',
-  handler: handlers.company.get,
-  options: {
-    auth: {
-      scope: ['admin', 'umana', 'company', 'candidate']
-    },
-    validate: {
-      query: schemas.company._get,
-      params: schemas.company._get_id
-    }
-  }
+	method: 'GET',
+	path: config.get('/api/base_path') + '/company/{id?}',
+	handler: handlers.company.get,
+	options: {
+		auth: {
+			scope: ['admin', 'umana', 'company', 'candidate']
+		},
+		validate: {
+			query: schemas.company._get,
+			params: schemas.company._get_id
+		}
+	}
 }, {
-  method: 'PUT',
-  path: config.get('/api/base_path') + '/company',
-  handler: handlers.company.update,
-  options: {
-    auth: {
-      scope: ['admin', 'umana', 'company', 'candidate']
-    },
-    validate: {
-      payload: schemas.company._update
-    }
-  }
+	method: 'PUT',
+	path: config.get('/api/base_path') + '/company/{id}',
+	handler: handlers.company.update,
+	options: {
+		auth: {
+			scope: ['admin', 'umana', 'company', 'candidate']
+		},
+		validate: {
+			payload: schemas.company._update,
+			params: schemas.company._get_id
+		}
+	}
 }, {
-
-  /**
-   * Company careers
-   */
-
-  method: 'POST',
-  path: config.get('/api/base_path') + '/career',
-  handler: handlers.company.career_add,
-  options: {
-    auth: {
-      scope: ['admin', 'umana', 'company', 'candidate']
-    },
-    validate: {
-      payload: schemas.company._career_add
-    }
-  }
+	
+	/**
+	 * Company careers
+	 */
+	
+	method: 'POST',
+	path: config.get('/api/base_path') + '/career',
+	handler: handlers.company.career_add,
+	options: {
+		auth: {
+			scope: ['admin', 'umana', 'company', 'candidate']
+		},
+		validate: {
+			payload: schemas.company._career_add
+		}
+	}
 }, {
-  method: 'GET',
-  path: config.get('/api/base_path') + '/career',
-  handler: handlers.company.career_get,
-  options: {
-    auth: {
-      scope: ['admin', 'umana', 'company', 'candidate']
-    },
-    validate: {
-      query: schemas.company._career_get
-    }
-  }
+	method: 'GET',
+	path: config.get('/api/base_path') + '/career',
+	handler: handlers.company.career_get,
+	options: {
+		auth: {
+			scope: ['admin', 'umana', 'company', 'candidate']
+		},
+		validate: {
+			query: schemas.company._career_get
+		}
+	}
 }, {
-  method: 'PUT',
-  path: config.get('/api/base_path') + '/career/{id?}',
-  handler: handlers.company.career_update,
-  options: {
-    auth: {
-      scope: ['admin', 'umana', 'company', 'candidate']
-    },
-    validate: {
-      payload: schemas.company._career_update,
-      params: schemas.company._get_id
-    }
-  }
+	method: 'PUT',
+	path: config.get('/api/base_path') + '/career/{id?}',
+	handler: handlers.company.career_update,
+	options: {
+		auth: {
+			scope: ['admin', 'umana', 'company', 'candidate']
+		},
+		validate: {
+			payload: schemas.company._career_update,
+			params: schemas.company._get_id
+		}
+	}
 }]
 
 module.exports = Company
