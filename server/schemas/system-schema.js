06fef52a87126aa286b87ac8147fcdead33e77be
Jorge Rojas <jorge.alberto.rojas.solorzano@gmail.com>
2020-07-13 12:06:19 -0600

    UPDATE add search schema

--- server/schemas/system-schema.js
+++ server/schemas/system-schema.js
@@ -3,16 +3,27 @@
 const Joi = require('@hapi/joi')
 
 const avatar_get = Joi.object({
-  filename: Joi.string().max(50).optional()
+	filename: Joi.string().max(50).optional()
 })
 
 const avatar_add = Joi.object({
-  entity_id: Joi.string().max(250).required(),
-  media: Joi.string().dataUri().required(),
-  module: Joi.string().valid(['companies', 'users', 'jobs']).required()
+	entity_id: Joi.string().max(250).required(),
+	media: Joi.string().dataUri().required(),
+	module: Joi.string().valid(['companies', 'users', 'jobs']).required()
+})
+
+const search = Joi.object({
+	type: Joi.string().valid('companies', 'users', 'jobs', 'careers'),
+	query: Joi.object({
+		name: Joi.string().allow(null).optional(),
+		variables: Joi.object({
+			limit: Joi.number().allow(null).optional()
+		})
+	})
 })
 
 module.exports = {
-  avatar_get,
-  avatar_add
+	avatar_get,
+	avatar_add,
+	search
 }
