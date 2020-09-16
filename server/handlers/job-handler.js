ed26a3c5176158fd3ed1da858a6c49bab4b7d4ef
Jorge Rojas <jorge.alberto.rojas.solorzano@gmail.com>
2020-09-16 17:55:20 -0600

    UPDATE Remove query & add helper

--- server/handlers/job-handler.js
+++ server/handlers/job-handler.js
@@ -1,13 +1,16 @@
 'use strict'
 
 const query = require('../query')
+const Boom = require('@hapi/boom')
+const Promise = require('bluebird')
+const helpers = require('../helpers')
 const table = 'jobs'
 
 module.exports = {
-  get: async (req, h) =>
-    h.response(await query.get(req, table)),
-  add: async (req, h) =>
-    h.response(await query.add(req, table)),
-  update: async (req, h) =>
-    h.response(await query.update(req, table))
+	get: async (req, h) =>
+		h.response(await helpers.get_jobs(req, table)),
+	add: async (req, h) =>
+		h.response(await query.add(req, table)),
+	update: async (req, h) =>
+		h.response(await query.update(req, table))
 }
