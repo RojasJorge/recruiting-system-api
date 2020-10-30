967b230856bd52c2b3bc1bbafe0fea9c475546d5
Jorge Rojas <jorge.alberto.rojas.solorzano@gmail.com>
2020-10-30 00:37:50 -0600

    UPDATE match step 1

--- server/handlers/match-handler.js
+++ server/handlers/match-handler.js
@@ -1,7 +1,22 @@
 'use strict'
 
-const candidates_to_job = (req, h) =>
-	h.response(true)
+const matching = require('../matching')
+const Promise = require('bluebird')
+
+const get_single_job = ({server: {db: {r, conn}}, params: {jobId}}) =>
+	new Promise((resolve, reject) => {
+		r
+			.table('jobs')
+			.get(jobId)
+			.run(conn, (err, result) => {
+				if(err) return reject(err)
+				
+				return resolve(result)
+			})
+	})
+
+const candidates_to_job = async (req, h) =>
+	h.response(await matching.do(req))
 
 module.exports = {
 	candidates_to_job
