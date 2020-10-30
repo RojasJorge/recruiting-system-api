8403d42fb71bb826590fbb72d41c6f14b7762573
Jorge Rojas <jorge.alberto.rojas.solorzano@gmail.com>
2020-10-30 00:39:55 -0600

    UPDATE Remove unused method

--- server/handlers/match-handler.js
+++ server/handlers/match-handler.js
@@ -1,19 +1,6 @@
 'use strict'
 
 const matching = require('../matching')
-const Promise = require('bluebird')
-
-const get_single_job = ({server: {db: {r, conn}}, params: {jobId}}) =>
-	new Promise((resolve, reject) => {
-		r
-			.table('jobs')
-			.get(jobId)
-			.run(conn, (err, result) => {
-				if(err) return reject(err)
-				
-				return resolve(result)
-			})
-	})
 
 const candidates_to_job = async (req, h) =>
 	h.response(await matching.do(req))
